c384d10772fe859038a7811ec237368424e9c88b
Ebaabuu <emaad.gafoor123@gmail.com>
2026-04-17 19:12:53 -0500

    Added Probing Question Format to Chat

--- brain.py
+++ brain.py
@@ -16,7 +16,16 @@ def load_rag_chain():
         
         embeddings = GoogleGenerativeAIEmbeddings(model="gemini-embedding-2-preview")
         vectorstore = Chroma(persist_directory="./chroma_db", embedding_function=embeddings)
-        retriever = vectorstore.as_retriever(search_kwargs={"k": 3}) 
+        # retriever = vectorstore.as_retriever(search_kwargs={"k": 3})
+        # Configure the retriever to use MMR
+        retriever = vectorstore.as_retriever(
+            search_type="mmr",
+            search_kwargs={
+                "k": 3,              # Number of chunks to eventually send to the AI
+                "fetch_k": 10,       # Number of chunks to initially fetch to find diversity
+                "lambda_mult": 0.5   # The balance: 1.0 is pure relevance, 0.0 is pure diversity
+        }
+)
         llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=0.3)
         
         system_prompt = (
@@ -24,6 +33,7 @@ def load_rag_chain():
             "Use the following pieces of retrieved context to answer the question. "
             "Use a friendly, helpful, and encouraging demeanor when talking to the user."
             "If the question is unclear, ask probing questions to guide the user.\n\n"
+            "You must always end your response by providing the Edwards IT phone number: 913-897-8652."
             "Context from Manuals:\n{context}\n\n"
             "Context from User Uploaded Files:\n{file_context}"
         )
